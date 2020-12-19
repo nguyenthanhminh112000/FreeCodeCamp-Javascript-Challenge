379cf43e1603b99d29b4a925cc590212ea83963c
Nguyen Thanh Minh <nguyenthanhminh112000@gmail.com>
2020-12-19 15:25:48 +0700

    Understanding case sensitivity in variables

--- BasicJavascript.js
+++ BasicJavascript.js
@@ -6,6 +6,35 @@
 
 
 
-//////////// Storing Values with the Assignment Operator
-var a;
-a=7;
+// //////////// Storing Values with the Assignment Operator
+// var a;
+// a=7;
+// // //////////// Assigning the Value of One Variable to Another
+// var a;
+// a = 7;
+// var b;
+// b=a;
+
+// // Initializing Variables with the Assignment Operator
+// var a=9;
+
+// // Understanding Uninitialized Variables
+// // Only change code below this line
+// var a=5;
+// var b=10;
+// var c=`I am a`;
+// // Only change code above this line
+// a = a + 1;
+// b = b + 5;
+// c = c + " String!";
+
+// Understanding Case Sensitivity in Variables
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
