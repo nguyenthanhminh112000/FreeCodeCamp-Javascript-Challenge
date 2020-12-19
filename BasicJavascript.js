63d3321992c30b04ab44d63f14e078f75556254e
Nguyen Thanh Minh <nguyenthanhminh112000@gmail.com>
2020-12-19 17:35:34 +0700

    Learn Javascript String

--- BasicJavascript.js
+++ BasicJavascript.js
@@ -103,3 +103,97 @@
 // a/=12;
 // b/=4;
 // c/=11;
+
+// // Declare String Variables
+// var myFirstName;
+// var myLastName;
+// myFirstName='Minh';
+// myLastName='Nguyen';
+// var _myFirstName;
+// var $myLastName;
+
+// // Escaping Literal Quotes in Strings
+// var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+// var myStr = `I am a "double quoted" string inside "double quotes".`; // Change this line
+
+// // Quoting Strings with Single Quotes
+// var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+// // Escape Sequences in Strings
+// var myStr;
+// myStr="FirstLine\n\t\\SecondLine\nThirdLine";
+
+// // Concatenating Strings with Plus Operator
+// var myStr='This is the start. '+'This is the end.';
+
+// // Concatenating Strings with the Plus Equals Operator
+// var myStr;
+// myStr='This is the first sentence. ';
+// myStr+='This is the second sentence.';
+
+// // Constructing Strings with Variables
+// // Only change code below this line
+// var myName;
+// var myStr;
+// myName='Minh';
+// myStr='My name is '+myName+' and I am well';
+
+// // Appending Variables to Strings
+// var someAdjective;
+// var myStr = "Learning to code is ";
+// someAdjective='awesome';
+// myStr+=someAdjective;
+
+// // Find the Length of a String
+// // Setup
+// var lastNameLength = 0;
+// var lastName = "Lovelace";
+// // Only change code below this line
+// lastNameLength = lastName.length;
+
+// // Use Bracket Notation to Find the First Character in a String
+// // Setup
+// var firstLetterOfLastName = "";
+// var lastName = "Lovelace";
+
+// // Only change code below this line
+// firstLetterOfLastName = lastName[0]; // Change this line
+
+// // Understand String Immutability
+// // Setup
+// var myStr = "Jello World";
+
+// // Only change code below this line
+// myStr= "Hello World"; // Change this line
+// // Only change code above this line
+
+// // Use Bracket Notation to Find the Nth Character in a String
+// // Setup
+// var lastName = "Lovelace";
+// // Only change code below this line
+// var thirdLetterOfLastName = lastName[2]; // Change this line
+
+// // Use Bracket Notation to Find the Last Character in a String
+// // Setup
+// var lastName = "Lovelace";
+
+// // Only change code below this line
+// var lastLetterOfLastName = lastName[lastName.length-1]; // Change this line
+
+// // Use Bracket Notation to Find the Nth-to-Last Character in a String
+// // Setup
+// var lastName = "Lovelace";
+
+// // Only change code below this line
+// var secondToLastLetterOfLastName = lastName[lastName.length-2]; // Change this line
+
+// // Word Blanks
+// var myNoun = "dog";
+// var myAdjective = "big";
+// var myVerb = "ran";
+// var myAdverb = "quickly";
+
+// // Only change code below this line
+// var wordBlanks =myAdjective+' '+myNoun+' '+myVerb+' '+myAdverb; // Change this line
+// // Only change code above this line
+// console.log(wordBlanks);
