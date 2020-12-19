0be49eac45b1a5b990631cf5a09906bc0b0ba691
Nguyen Thanh Minh <nguyenthanhminh112000@gmail.com>
2020-12-19 15:03:14 +0700

    Storing values with the assignment operator

--- BasicJavascript.js
+++ BasicJavascript.js
@@ -1,4 +1,11 @@
 //////////// Declare JavaScript Variables
 
 // Note : variable can contain numbers,letters,$,_ but cant start with number and contain space
-var myName;
+// var myName;
+
+
+
+
+//////////// Storing Values with the Assignment Operator
+var a;
+a=7;
