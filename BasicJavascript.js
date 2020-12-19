b4d8566bf16580c04b95b777f49a548caf0b50dd
Nguyen Thanh Minh <nguyenthanhminh112000@gmail.com>
2020-12-19 15:38:40 +0700

    Basic Math Operator

--- BasicJavascript.js
+++ BasicJavascript.js
@@ -28,13 +28,36 @@
 // b = b + 5;
 // c = c + " String!";
 
-// Understanding Case Sensitivity in Variables
-// Variable declarations
-var studlyCapVar;
-var properCamelCase;
-var titleCaseOver;
+// // Understanding Case Sensitivity in Variables
+// // Variable declarations
+// var studlyCapVar;
+// var properCamelCase;
+// var titleCaseOver;
 
-// Variable assignments
-studlyCapVar = 10;
-properCamelCase = "A String";
-titleCaseOver = 9000;
+// // Variable assignments
+// studlyCapVar = 10;
+// properCamelCase = "A String";
+// titleCaseOver = 9000;
+
+// // Add Two Numbers with JavaScript
+// var sum = 10 + 10;
+
+// // Subtract One Number from Another with JavaScript
+// var difference = 45 - 33;
+
+// // Multiply Two Numbers with JavaScript
+// var product = 8 * 10;
+
+// // Divide One Number by Another with JavaScript
+// var quotient = 66 / 33;
+
+// // Increment a Number with JavaScript
+// var myVar = 87;
+// // Only change code below this line
+// myVar++;
+
+// // Decrement a Number with JavaScript
+// var myVar = 11;
+// // Only change code below this line
+// myVar--;
+
