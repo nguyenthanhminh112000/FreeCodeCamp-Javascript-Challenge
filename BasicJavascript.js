4501dadf7730939aca957994082e02832ab2baae
Nguyen Thanh Minh <nguyenthanhminh112000@gmail.com>
2020-12-19 18:12:59 +0700

    Learn Javascript Array

--- BasicJavascript.js
+++ BasicJavascript.js
@@ -197,3 +197,71 @@
 // var wordBlanks =myAdjective+' '+myNoun+' '+myVerb+' '+myAdverb; // Change this line
 // // Only change code above this line
 // console.log(wordBlanks);
+
+// //  Store Multiple Values in one Variable using JavaScript Arrays
+// // Only change code below this line
+// var myArray = ['Nguyen Thanh Minh',20];
+// console.log(myArray);
+
+// // Nest one Array within Another Array
+// var myArray=[["Bulls", 23], ["White Sox", 45,13]];
+// for(let i=0;i<myArr.length;i++){
+//     for(let j=0;j<myArr[i].length;j++){
+//         console.log(myArr[i][j]);
+//     }
+// }
+// console.log(myArray);
+
+// Access Array Data with Indexes
+// Setup
+// var myArray = [50,60,70];
+// // Only change code below this line
+// var myData=myArray[0];
+
+// // Modify Array Data With Indexes
+// // Setup
+// var myArray = [18,64,99];
+// // Only change code below this line
+// myArray[0]=45;
+
+// // Access Multi-Dimensional Arrays With IndexesPassed
+// // Setup
+// var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+// // Only change code below this line
+// var myData = myArray[2][1];
+
+// // Manipulate Arrays With push()
+// // Setup
+// var myArray = [["John", 23], ["cat", 2]];
+// // Only change code below this line
+// myArray.push(["dog", 3]);
+
+// // Manipulate Arrays With pop()
+// // Setup
+// var myArray = [["John", 23], ["cat", 2]];
+// // Only change code below this line
+// var removedFromMyArray=myArray.pop();
+
+// // Manipulate Arrays With shift()
+// // Setup
+// var myArray = [["John", 23], ["dog", 3]];
+
+// // Only change code below this line
+// var removedFromMyArray=myArray.shift();
+
+// // Manipulate Arrays With unshift()
+// // Setup
+// var myArray = [["John", 23], ["dog", 3]];
+// myArray.shift();
+
+// // Only change code below this line
+// myArray.unshift(["Paul",35]);
+
+// // Shopping List
+// var myList = [];
+// myList.push(['Chocolate Bar',15]);
+// myList.push(['Chocolate Bar',15]);
+// myList.push(['Chocolate Bar',15]);
+// myList.push(['Chocolate Bar',15]);
+// myList.push(['Chocolate Bar',15]);
