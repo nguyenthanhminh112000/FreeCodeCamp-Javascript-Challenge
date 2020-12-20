c38df0f2dfc36ed157e86d4155fe051c503494aa
Nguyen Thanh Minh <nguyenthanhminh112000@gmail.com>
2020-12-20 14:01:38 +0700

    Learn Javascript Object

--- BasicJavascript.js
+++ BasicJavascript.js
@@ -744,32 +744,190 @@
 //   }
 //   abTest(2,2);
 
-// Counting Cards
-var count = 0;
-function cc(card) {
-  // Only change code below this line
-  switch(card){
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-        count++;
-        break;
-    case 7:
-    case 8:
-    case 9:
-        count+=0;
-        break;
-    case 10:
-    case 'J':
-    case 'Q':
-    case 'K':
-    case 'A':
-        count-=1;
-  }
-  return `${count} ${count>0?'Bet':'Hold'}`;
-  // Only change code above this line
-}
+// // Counting Cards
+// var count = 0;
+// function cc(card) {
+//   // Only change code below this line
+//   switch(card){
+//     case 2:
+//     case 3:
+//     case 4:
+//     case 5:
+//     case 6:
+//         count++;
+//         break;
+//     case 7:
+//     case 8:
+//     case 9:
+//         count+=0;
+//         break;
+//     case 10:
+//     case 'J':
+//     case 'Q':
+//     case 'K':
+//     case 'A':
+//         count-=1;
+//   }
+//   return `${count} ${count>0?'Bet':'Hold'}`;
+//   // Only change code above this line
+// }
 
-cc(2); cc(3); cc(7); cc('K'); cc('A');
+// cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+// // Build JavaScript Objects
+// var myDog = {
+//     // Only change code below this line
+//     "name": "Whiskers",
+//     "legs": 4,
+//     "tails": 1,
+//     "friends": ["Water", "Dogs"]
+//     // Only change code above this line
+//     };
+
+// // Accessing Object Properties with Dot Notation
+// // Setup
+// var testObj = {
+//     "hat": "ballcap",
+//     "shirt": "jersey",
+//     "shoes": "cleats"
+//   };
+  
+//   // Only change code below this line
+  
+//   var hatValue = testObj.hat;      // Change this line
+//   var shirtValue = testObj.shirt;    // Change this line
+
+// // Accessing Object Properties with Bracket Notation
+// // Setup
+// var testObj = {
+//     "an entree": "hamburger",
+//     "my side": "veggies",
+//     "the drink": "water"
+//   };
+  
+//   // Only change code below this line
+  
+//   var entreeValue = testObj['an entree'];   // Change this line
+//   var drinkValue = testObj['the drink'];    // Change this line
+
+// // Accessing Object Properties with Variables
+// // Setup
+// var testObj = {
+//     12: "Namath",
+//     16: "Montana",
+//     19: "Unitas"
+//   };
+  
+//   // Only change code below this line
+  
+//   var playerNumber=16;       // Change this line
+//   var player = testObj[playerNumber];   // Change this line
+
+// // Updating Object Properties
+// // Setup
+// var myDog = {
+//     "name": "Coder",
+//     "legs": 4,
+//     "tails": 1,
+//     "friends": ["freeCodeCamp Campers"]
+//   };
+  
+//   // Only change code below this line
+//   myDog.name=`Happy Coder`;
+
+//   // Add New Properties to a JavaScript Object
+//   // Setup
+// var myDog = {
+//     "name": "Happy Coder",
+//     "legs": 4,
+//     "tails": 1,
+//     "friends": ["freeCodeCamp Campers"]
+//   };
+  
+//   // Only change code below this line
+//   myDog.bark='woof';
+
+//   // Delete Properties from a JavaScript Object 
+//   // Setup
+// var myDog = {
+//     "name": "Happy Coder",
+//     "legs": 4,
+//     "tails": 1,
+//     "friends": ["freeCodeCamp Campers"],
+//     "bark": "woof"
+//   };
+//   // Only change code below this line
+//   delete myDog.tails;
+
+// // Using Objects for Lookups
+// // Setup
+// function phoneticLookup(val) {
+//     var result = "";
+  
+//     // Only change code below this line
+//     var lookup={
+//         alpha:`Adams`,
+//         bravo:`Boston`,
+//         charlie:`Chicago`,
+//         delta:`Denver`,
+//         echo:`Easy`,
+//         foxtrot:`Frank`
+//     };
+//     result=lookup[val];
+  
+//     // Only change code above this line
+//     return result;
+//   }
+  
+//   phoneticLookup("charlie");
+
+// // Testing Objects for Properties
+// function checkObj(obj, checkProp) {
+//     // Only change code below this line
+//     ;
+//     return obj.hasOwnProperty(checkProp)?obj[checkProp]:`Not Found`;
+//     // Only change code above this line
+// }
+
+// //  Manipulating Complex Objects
+// var myMusic = [
+//     {
+//       "artist": "Billy Joel",
+//       "title": "Piano Man",
+//       "release_year": 1973,
+//       "formats": [
+//         "CD",
+//         "8T",
+//         "LP"
+//       ],
+//       "gold": true
+//     },
+//     // Add a record here
+//     {
+//         "artist": "Daft Punk",
+//         "title": "Homework",
+//         "release_year": 1997,
+//         "formats": [ 
+//           "CD",
+//           "Cassette",
+//           "LP"
+//         ],
+//         "gold": true
+//       }
+//   ];
+
+//   // Accessing Nested Objects
+//   // Setup
+// var myStorage = {
+//     "car": {
+//       "inside": {
+//         "glove box": "maps",
+//         "passenger seat": "crumbs"
+//        },
+//       "outside": {
+//         "trunk": "jack"
+//       }
+//     }
+//   };
+  
+//   var gloveBoxContents = myStorage.car.inside[`glove box`]; // Change this line
