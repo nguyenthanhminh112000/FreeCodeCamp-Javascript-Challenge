b9360cfb40b68f6d58cc9ee8d9d20d4bfac3d6d5
Nguyen Thanh Minh <nguyenthanhminh112000@gmail.com>
2020-12-21 14:48:38 +0700

    Recursion

--- BasicJavascript.js
+++ BasicJavascript.js
@@ -1072,4 +1072,13 @@
 //     i++;
 // }while(i<10)
 
-// Replace Loops using Recursion
+// // Replace Loops using Recursion
+// function sum(arr, n) {
+//     // Only change code below this line
+//     if(n<=0){
+//         return 0;
+//     }else{
+//         return sum(arr,n-1)+arr[n-1];
+//     }
+//     // Only change code above this line
+// }
