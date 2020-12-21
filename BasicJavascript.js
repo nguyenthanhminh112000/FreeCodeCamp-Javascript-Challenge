e9b4bf091ffd87dd3b90c7176b0cc7aa7878c9cc
Nguyen Thanh Minh <nguyenthanhminh112000@gmail.com>
2020-12-21 14:30:22 +0700

    Loop by while,do-while,for

--- BasicJavascript.js
+++ BasicJavascript.js
@@ -997,3 +997,79 @@
 //     return object;
 //   }
 //   updateRecords(collection, 5439, 'artist', 'ABBA');
+
+// // Iterate with JavaScript While Loops
+// // Setup
+// var myArray = [];
+
+// // Only change code below this line
+// let i=5;
+// while(i>=0){
+//     myArray.push(i);
+//     i--;
+// }
+
+// // Iterate with JavaScript For Loops
+// // Setup
+// var myArray = [];
+
+// // Only change code below this line
+// for(let i=1;i<=5;i++){
+//     myArray.push(i);
+// }
+
+// // Iterate Odd Numbers With a For Loop
+// // Setup
+// var myArray = [];
+
+// // Only change code below this line
+// for(let i=1;i<=9;i+=2){
+//     myArray.push(i);
+// }
+
+// // Count Backwards With a For Loop
+// // Setup
+// var myArray = [];
+
+// // Only change code below this line
+// for(let i=9;i>=1;i-=2){
+//     myArray.push(i);
+// }
+
+// // Iterate Through an Array with a For Loop
+// // Setup
+// var myArr = [ 2, 3, 4, 5, 6];
+
+// // Only change code below this line
+// let total=0;
+// for(let i=0;i<myArr.length;i++){
+//     total+=myArr[i];
+// }
+
+// // Nesting For Loops
+// function multiplyAll(arr) {
+//     var product = 1;
+//     // Only change code below this line
+//     for(let i=0;i<arr.length;i++){
+//         for(let j=0;j<arr[i].length;j++){
+//             product*=arr[i][j];
+//         }
+//     }
+//     // Only change code above this line
+//     return product;
+//   }  
+// multiplyAll([[1,2],[3,4],[5,6,7]]);
+
+// // Iterate with JavaScript Do...While Loops
+// // Setup
+// var myArray = [];
+// var i = 10;
+
+// // Only change code below this line
+
+// do {
+//     myArray.push(i);
+//     i++;
+// }while(i<10)
+
+// Replace Loops using Recursion
