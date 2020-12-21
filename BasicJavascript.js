4c0786f3c1175b94ba2fc4e9391cdbefaaa81906
Nguyen Thanh Minh <nguyenthanhminh112000@gmail.com>
2020-12-21 15:39:00 +0700

    Random whole numbers with javascript

--- BasicJavascript.js
+++ BasicJavascript.js
@@ -1083,50 +1083,68 @@
 //     // Only change code above this line
 // }
 
-// Profile Lookup
-// Setup
-var contacts = [
-    {
-        "firstName": "Akira",
-        "lastName": "Laine",
-        "number": "0543236543",
-        "likes": ["Pizza", "Coding", "Brownie Points"]
-    },
-    {
-        "firstName": "Harry",
-        "lastName": "Potter",
-        "number": "0994372684",
-        "likes": ["Hogwarts", "Magic", "Hagrid"]
-    },
-    {
-        "firstName": "Sherlock",
-        "lastName": "Holmes",
-        "number": "0487345643",
-        "likes": ["Intriguing Cases", "Violin"]
-    },
-    {
-        "firstName": "Kristian",
-        "lastName": "Vos",
-        "number": "unknown",
-        "likes": ["JavaScript", "Gaming", "Foxes"]
-    }
-];
+// // Profile Lookup
+// // Setup
+// var contacts = [
+//     {
+//         "firstName": "Akira",
+//         "lastName": "Laine",
+//         "number": "0543236543",
+//         "likes": ["Pizza", "Coding", "Brownie Points"]
+//     },
+//     {
+//         "firstName": "Harry",
+//         "lastName": "Potter",
+//         "number": "0994372684",
+//         "likes": ["Hogwarts", "Magic", "Hagrid"]
+//     },
+//     {
+//         "firstName": "Sherlock",
+//         "lastName": "Holmes",
+//         "number": "0487345643",
+//         "likes": ["Intriguing Cases", "Violin"]
+//     },
+//     {
+//         "firstName": "Kristian",
+//         "lastName": "Vos",
+//         "number": "unknown",
+//         "likes": ["JavaScript", "Gaming", "Foxes"]
+//     }
+// ];
 
 
-function lookUpProfile(name, prop){
-// Only change code below this line
-    for(let i=0;i<contacts.length;i++){
-        if(contacts[i].firstName===name){
-            if(contacts[i].hasOwnProperty(prop)){
-                return contacts[i][prop];
-            }else{
-                return `No such property`;
-            }
-        }
-    }
-    return `No such contact`;
+// function lookUpProfile(name, prop){
+// // Only change code below this line
+//     for(let i=0;i<contacts.length;i++){
+//         if(contacts[i].firstName===name){
+//             if(contacts[i].hasOwnProperty(prop)){
+//                 return contacts[i][prop];
+//             }else{
+//                 return `No such property`;
+//             }
+//         }
+//     }
+//     return `No such contact`;
 
-// Only change code above this line
-}
+// // Only change code above this line
+// }
 
-lookUpProfile("Akira", "likes");
+// lookUpProfile("Akira", "likes");
+
+// // Generate Random Fractions with JavaScript
+// function randomFraction() {
+
+//     // Only change code below this line
+  
+//     return Math.random();
+  
+//     // Only change code above this line
+//   }
+
+// Generate Random Whole Numbers with JavaScript
+function randomWholeNum() {
+
+    // Only change code below this line
+  
+    return Math.floor(Math.random()*10);
+  }
